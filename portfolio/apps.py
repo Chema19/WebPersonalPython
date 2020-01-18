982dca39d9c83196c299f4c2c10263696dde0683
Chema19 <chemalonso96@gmail.com>
2020-01-18 17:43:35 -0500

    finalizacion

--- portfolio/apps.py
+++ portfolio/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class PortfolioConfig(AppConfig):
     name = 'portfolio'
+    verbose_name = 'Portafolio'#configuracion para cambiar a español
--- portfolio/models.py
+++ portfolio/models.py
@@ -2,8 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    image = models.ImageField()
-    created = models.DateTimeField(auto_now_add=True)#Se ejecuta cada vez que se registra
-    updated = models.DateTimeField(auto_now=True)#Se ejecuta cada vez que se actualiza
+    title = models.CharField(max_length=200, verbose_name='Titulo')
+    description = models.TextField(verbose_name='Descripción')
+    image = models.ImageField(verbose_name='Imagen', upload_to='projects')
+    link = models.URLField(verbose_name='Direccion Web', null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creación')#Se ejecuta cada vez que se registra
+    updated = models.DateTimeField(auto_now=True, verbose_name='Fecha de actualización')#Se ejecuta cada vez que se actualiza
+
+    class Meta:
+        verbose_name = "proyecto"
+        verbose_name_plural = "proyectos"
+        ordering = ["-created"]#ordenar la lista de proyectos el "-" es para ordenarlo del primero al ultimo o biceversa
+        
+    def __str__(self):
+        return self.title#cambia el nombre el titulo del proyecto para no mn
